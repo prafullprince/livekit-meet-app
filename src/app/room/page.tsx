0365e1b6f39f06b1bd59d858c21295a708b01b5f
Prafull Prince <prafullprince12@gmail.com>
2025-06-17 15:50:40 +0530

    a

---
+++ src/app/room/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const page = () => {
+    redirect('/')
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
